069c7b2508c806b67f541e0de296b68ec46ccfef
DTH235650_LEPHUCDUCHIEN <lpdhien16@gmail.com>
2025-09-17 22:33:00 +0700

    Update Cau10.py

--- BaiTapChuong3/Cau10.py
+++ BaiTapChuong3/Cau10.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 x=int(input("Nhập x:"))
 n=int(input("Nhập n:"))
 s=0
@@ -9,13 +8,3 @@ for i in range(1,n+1):
         mau=mau*j
     s=s+(tu/mau)
 print("s({0},{1})={2}".format(x,n,s))
-=======
-print("          *     ")
-print("        * * *")
-print("    * * * * * * *")
-print("        * * *")
-print("      * * * * *")
-print("* * * * * * * * * * *")
-print("        *   *")
-print("        *   *")
->>>>>>> c0de2ac0eb83faaf12050d954f3f6ef691c199bb
