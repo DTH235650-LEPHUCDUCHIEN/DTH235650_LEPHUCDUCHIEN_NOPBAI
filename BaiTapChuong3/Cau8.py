1275b4bfe89ab62ef246e7562f53a0f0f1f25143
DTH235650_LEPHUCDUCHIEN <lpdhien16@gmail.com>
2025-09-17 22:35:16 +0700

    Update Cau8.py

--- BaiTapChuong3/Cau8.py
+++ BaiTapChuong3/Cau8.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = float(input("Nhập vào giá trị a = "))
 b = float(input("Nhập vào giá trị b = "))
 operation = input("Nhập phép tính (+,-,*,/): ")
@@ -20,16 +19,3 @@ elif operation == "/":
         print("Lỗi: Không thể chia hết cho 0.")
 else:
     print("Phép toán không hợp lệ.")
-=======
-width = 4   # chiều ngang
-height = 4  # chiều dọc
-
-for i in range(height):
-    for j in range(width):
-        # nếu là dòng đầu, dòng cuối, hoặc cột đầu, cột cuối -> in '*'
-        if i == 0 or i == height - 1 or j == 0 or j == width - 1:
-            print('*', end=' ')
-        else:
-            print(' ', end=' ')
-    print()
->>>>>>> c0de2ac0eb83faaf12050d954f3f6ef691c199bb
